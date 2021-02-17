accf470523776eda74adba6d535cb2bacf6998ab
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-17 14:52:15 -0500

    Removed unused and commented out code

--- js/main.js
+++ js/main.js
@@ -27,72 +27,8 @@ let featuresImg3 = document.getElementById('featuresImg3');
 //carousel for features section-----------------------------------
 
 
-// let tab = document.getElementById("cat1-wrap");
-// let tab2 = document.getElementById("cat2-wrap");
-// let tab3 = document.getElementById("cat3-wrap");
 
-
-// let featImg = document.getElementById("indFeaturesImg");
-// let featImg2 = document.getElementById("indFeaturesImg2");
-// let featImg3 = document.getElementById("indFeaturesImg3");
-
-// let indicator1 = document.querySelector(".divider1");
-// let indicator2 = document.querySelector(".divider2");
-// let indicator3 = document.querySelector(".divider3");
-
-
-// featImg2.style.display = "none";
-// featImg3.style.display = "none";
-// indicator1.style.display = "block";
-// indicator2.style.display = "none";
-// indicator3.style.display = "none";
-// let amountToMove = "-500px";
-
-
-// function slider() {
-//   featImg.style.display = "flex";
-//   featImg2.style.display = "none";
-//   featImg3.style.display = "none";
-//   indicator1.style.display = "block";
-//   indicator2.style.display = "none";
-//   indicator3.style.display = "none";
-
-//   featImg.style.transform = "translateX(-" + amountToMove + ")";
-// }
-
-
-// tab.addEventListener("click", slider);
-
-
-// function slider2() {
-//   featImg.style.display = "none";
-//   featImg2.style.display = "flex";
-//   featImg3.style.display = "none";
-//   indicator1.style.display = "none";
-//   indicator2.style.display = "block";
-//   indicator3.style.display = "none";
-
-//   featImg2.style.transform = "translateX(-" + amountToMove + ")";
-// }
-
-// tab2.addEventListener("click", slider2);
-
-// function slider3() {
-//   featImg.style.display = "none";
-//   featImg2.style.display = "none";
-//   featImg3.style.display = "flex";
-//   indicator1.style.display = "none";
-//   indicator2.style.display = "none";
-//   indicator3.style.display = "block";
-
-//   featImg2.style.transform = "translateX(-" + amountToMove + ")";
-// }
-
-
-// tab3.addEventListener("click", slider3);
-
-
-$(document).ready(()=>{
+$(document).ready(() => {
 
   $('#indFeaturesImg2').hide();
   $('#indFeaturesImg3').hide();
@@ -100,151 +36,37 @@ $(document).ready(()=>{
   $('.divider2').hide();
   $('.divider3').hide();
 
-$('#cat1-wrap').on('click', ()=> {
-  $('#indFeaturesImg').fadeIn(600);
-  $('#indFeaturesImg2').hide();
-  $('#indFeaturesImg3').hide();
-  $('.divider1').show();
-  $('.divider2').hide();
-  $('.divider3').hide();
+  $('#cat1-wrap').on('click', () => {
+    $('#indFeaturesImg').fadeIn(600);
+    $('#indFeaturesImg2').hide();
+    $('#indFeaturesImg3').hide();
+    $('.divider1').show();
+    $('.divider2').hide();
+    $('.divider3').hide();
+  })
+
+  $('#cat2-wrap').on('click', () => {
+    $('#indFeaturesImg').hide();
+    $('#indFeaturesImg2').fadeIn(600);
+    $('#indFeaturesImg3').hide();
+    $('.divider1').hide();
+    $('.divider2').show();
+    $('.divider3').hide();
+  })
+
+  $('#cat3-wrap').on('click', () => {
+    $('#indFeaturesImg').hide();
+    $('#indFeaturesImg2').hide();
+    $('#indFeaturesImg3').fadeIn(600);
+    $('.divider1').hide();
+    $('.divider2').hide();
+    $('.divider3').show();
+  })
+
 })
 
-$('#cat2-wrap').on('click', ()=>{
-  $('#indFeaturesImg').hide();
-  $('#indFeaturesImg2').fadeIn(600);
-  $('#indFeaturesImg3').hide();
-  $('.divider1').hide();
-  $('.divider2').show();
-  $('.divider3').hide();
-})
-
-$('#cat3-wrap').on('click', ()=>{
-  $('#indFeaturesImg').hide();
-  $('#indFeaturesImg2').hide();
-  $('#indFeaturesImg3').fadeIn(600);
-  $('.divider1').hide();
-  $('.divider2').hide();
-  $('.divider3').show();
-})
-
-})
 
 
-
-
-
-//function to open and close the faq questions section 
-// function openClose(){
-//     if(para1.style.display === "none"){
-//         para1.style.display = "block";
-//         arrowClose.style.display = "none";
-//         arrowOpen.style.display ="block";
-
-//         para2.style.display = "none";
-//         arrowClose2.style.display = "block";
-//         arrowOpen2.style.display ="none";
-
-//         para3.style.display = "none";
-//         arrowClose3.style.display = "block";
-//         arrowOpen3.style.display ="none";
-
-//         para4.style.display = "none";
-//         arrowClose4.style.display = "block";
-//         arrowOpen4.style.display ="none";
-
-//     } else {
-//         para1.style.display = "none";
-//         arrowOpen.style.display="none";
-//         arrowClose.style.display ="block";
-
-//     }
-// }
-
-// openClose();
-
-// function openClose2(){
-//     if(para2.style.display === "none"){
-//         para2.style.display = "block";
-//         arrowClose2.style.display = "none";
-//         arrowOpen2.style.display ="block";
-
-//         para1.style.display = "none";
-//         arrowClose.style.display = "block";
-//         arrowOpen.style.display ="none";
-
-//         para3.style.display = "none";
-//         arrowClose3.style.display = "block";
-//         arrowOpen3.style.display ="none";
-
-//         para4.style.display = "none";
-//         arrowClose4.style.display = "block";
-//         arrowOpen4.style.display ="none";
-
-
-//     } else {
-//         para2.style.display = "none";
-//         arrowOpen2.style.display="none";
-//         arrowClose2.style.display ="block";
-//     }
-// }
-
-// openClose2();
-
-// function openClose3(){
-//     if(para3.style.display === "none"){
-//         para3.style.display = "block";
-//         arrowClose3.style.display = "none";
-//         arrowOpen3.style.display ="block";
-
-//         para1.style.display = "none";
-//         arrowClose.style.display = "block";
-//         arrowOpen.style.display ="none";
-
-//         para2.style.display = "none";
-//         arrowClose2.style.display = "block";
-//         arrowOpen2.style.display ="none";
-
-//         para4.style.display = "none";
-//         arrowClose4.style.display = "block";
-//         arrowOpen4.style.display ="none";
-
-
-//     } else {
-//         para3.style.display = "none";
-//         arrowOpen3.style.display="none";
-//         arrowClose3.style.display ="block";
-//     }
-// }
-
-// openClose3();
-
-
-// function openClose4(){
-//     if(para4.style.display === "none"){
-//         para4.style.display = "block";
-//         arrowClose4.style.display = "none";
-//         arrowOpen4.style.display ="block";
-
-//         para3.style.display = "none";
-//         arrowClose3.style.display = "block";
-//         arrowOpen3.style.display ="none";
-
-//         para1.style.display = "none";
-//         arrowClose.style.display = "block";
-//         arrowOpen.style.display ="none";
-
-//         para2.style.display = "none";
-//         arrowClose2.style.display = "block";
-//         arrowOpen2.style.display ="none";
-
-//     } else {
-//         para4.style.display = "none";
-//         arrowOpen4.style.display="none";
-//         arrowClose4.style.display ="block";
-//     }
-// }
-
-// openClose4();
 
 $(document).ready(() => {
   //the first faq question and paragraph
